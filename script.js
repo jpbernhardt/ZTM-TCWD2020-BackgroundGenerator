975adc2cd634cca1cdf0bfee17ca0e984f537693
Jean-Patrick Bernhardt <jp.bernhardt@gmail.com>
2020-08-11 19:45:58 +0200

    Added random color generator. Gradient uses color picker values during first page load. Color value information is now also displayed during first page load.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var button = document.querySelector(".randomGenBtn");
 var body = document.getElementById("gradient");
 
 var setGradient = () => {
@@ -14,6 +15,32 @@ var setGradient = () => {
   css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
+// Random hex color code generator.
+var randomHexColorCode = () => {
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
+  var newColorCode = "#" + randomColor;
+  return newColorCode;
+}
 
+/* Calls the randomHexColorCode function, adds and updates the background
+and color information with the new values. */
+var randomColor = () => {
+  color1.value = randomHexColorCode();
+  color2.value = randomHexColorCode();
+
+  body.style.background =
+  "linear-gradient(to right, "
+  + color1.value
+  + ", "
+  + color2.value
+  + ")";
+
+  css.textContent = body.style.background + ";";
+}
+
+
+
+color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+button.addEventListener("click", randomColor);
+randomColor();
